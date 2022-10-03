53d55d4cfec877ac219d3f7fd3e21f1b45883716
Hyma <hymadasetty22@gmail.com>
2022-10-03 11:11:16 +0530

    initial commit

---
+++ src/test/java/sampletestcases/LoginTests.java
@@ -0,0 +1,32 @@
+package sampletestcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTests {
+
+@Test
+public void verifyloginwithvalidcredential() {
+	
+	 System.out.println(" Test starting");	 
+	 Assert.assertTrue(true);
+	 System.out.println("Test End");
+}
+@Test
+public void verifyloginwithInvalidcredential() {
+	 System.out.println(" Test starting");	 
+	 Assert.assertTrue(true);
+	 System.out.println("Test End");
+}
+
+@Test
+public void verifyloginwithnocredential() {
+System.out.println(" Test starting");	 
+Assert.assertTrue(true);
+System.out.println("Test End");
+
+}
+
+
+
+}
